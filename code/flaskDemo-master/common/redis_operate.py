5cdc5b0e2f28a747dff497615758bf40cfa12b87
2431011g <60514079+2431011g@users.noreply.github.com>
2021-11-17 22:56:02 +0000

    Update redis_operate.py

--- code/flaskDemo-master/common/redis_operate.py
+++ code/flaskDemo-master/common/redis_operate.py
@@ -5,18 +5,18 @@ from config.setting import REDIS_HOST, REDIS_PORT, REDIS_PASSWD, EXPIRE_TIME
 class RedisDb():
 
     def __init__(self, host, port, passwd):
-        # 建立数据库连接
+        # Establish a database connection
         self.r = redis.Redis(
             host=host,
             port=port,
             password=passwd,
-            decode_responses=True # get() 得到字符串类型的数据
+            decode_responses=True # get() Get string type data
         )
 
     def handle_redis_token(self, key, value=None):
-        if value: # 如果value非空，那么就设置key和value，EXPIRE_TIME为过期时间
+        if value:# If the value is not empty, then set the key and value, EXPIRE_TIME is the expiration time
             self.r.set(key, value, ex=EXPIRE_TIME)
-        else: # 如果value为空，那么直接通过key从redis中取值
+        else: # If the value is empty, then directly get the value from redis through the key
             redis_token = self.r.get(key)
             return redis_token
 
